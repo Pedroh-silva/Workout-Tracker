768a4ea1754e2e7b68949fdef4bde310baaf480c
Pedro Silva <pehenriquesilva1202@gmail.com>
2022-09-13 21:48:14 -0300

    Home page and dark mode finished

---
+++ wwwroot/js/index.js
@@ -0,0 +1,46 @@
+﻿$(document).ready(function () {
+    var fade = $('#fade');
+    var modal = $('.modal');
+    var conteudoModal = $('#conteudo');
+    var conteudoModalExercicio = $('#conteudoExercicio');
+    var botaoEditar = $('.editar');
+    var botaoRemover = $('.remover');
+    $('.fa-pen').on('click', function () {
+        var id = $(this).attr('itemid');
+        botaoEditar.attr('href',"/Workouts/Edit/" + id);
+        botaoRemover.attr('href',"/Workouts/Delete/" + id);
+        //animação
+        fade.css('display', 'flex');
+        modal.css('display', 'block');
+        modal.css('animation', 'scale-up 0.6s forwards')
+        setTimeout(function () {
+            conteudoModal.fadeIn();
+        }, 310);
+        //-----------------------------------------
+        
+    });
+    $('#closeModal').on('click', function () {
+        modal.css('animation', 'scale-down 0.4s forwards');
+        conteudoModal.fadeOut("fast");
+        conteudoModalExercicio.fadeOut("fast");
+        setTimeout(function () {
+            modal.css('display', 'none');
+            fade.css('display', 'none');
+        }, 310);
+        
+        
+    });
+    $('.exercicios').on('click', function () {
+        var id = $(this).attr('itemid');
+        var lista = $('#exerciseList_' + id).html();
+        $('#exercisesSelectedWorkout').html(lista);
+        //animação
+        fade.css('display', 'flex');
+        modal.css('display', 'block');
+        modal.css('animation', 'scale-up 0.6s forwards')
+        setTimeout(function () {
+            conteudoModalExercicio.fadeIn();
+        }, 310);
+        //-----------------------------------------
+    });
+});
--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,30 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+var theme = localStorage.getItem('theme');
+if (theme == 'dark') {
+    $('html').addClass('darkmode');
+    $('#colorTheme').prop('checked', false);
+    $('.lightDarkMode').fadeOut(function () {
+        $(this).html("<i class='fas fa-moon'></i>")
+    }).fadeIn();
+    localStorage.setItem('theme', 'dark');
+}
+$('#colorTheme').on('click', function () {
+    theme = localStorage.getItem('theme');
+    if (theme == null) {
+        $('html').addClass('darkmode');
+        $('#colorTheme').prop('checked', false);
+        $('.lightDarkMode').fadeOut("fast", function () {
+            $(this).html("<i class='fas fa-moon'></i>")
+        }).fadeIn("fast");
+        localStorage.setItem('theme', 'dark');
+    }
+    else {
+        $('html').removeClass('darkmode');
+        $('.lightDarkMode').fadeOut("fast", function () {
+            $(this).html("<i class='fas fa-sun'></i>")
+        }).fadeIn("fast");
+        localStorage.removeItem('theme');
+    }
+});
